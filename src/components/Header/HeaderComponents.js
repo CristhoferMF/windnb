6a81fee7541393c3c18e41ead164a6fc5365ab66
Cristhofer <cristhofermontalvo@gmail.com>
2021-07-10 15:39:17 -0500

    Header and Form Search

--- src/components/Header/HeaderComponents.js
+++ src/components/Header/HeaderComponents.js
@@ -2,7 +2,12 @@ import styled from "styled-components";
 import {colors, screens} from '../../config/theme'
 
 export const HeaderContainer = styled.div`
-    
+    font-family: 'Mulish' sans-serif;
+
+    div,input,button,label {
+        font-family: 'Mulish';
+        font-size: 14px;
+    }
 `
 
 export const HeaderWrapper = styled.div`
@@ -40,7 +45,6 @@ export const SearchWidgetWrapper = styled.div`
     height: 55px;
     box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
     border-radius: 16px;
-    font-family: 'Mulish';
     max-width: min-content;
     display: flex;
     align-self: center;
@@ -51,14 +55,14 @@ export const SearchWidgetWrapper = styled.div`
         box-shadow: 0px 2px 6px rgba(0, 0, 0, 0.2);
     }
 `
-export const InputLocation = styled.div`
+export const LocationText = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
     width: 140px;
     border-right: 1px solid #F2F2F2;
 `
-export const InputGuests = styled.div`
+export const GuestsText = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -72,4 +76,143 @@ export const SearchIconWrapper = styled.div`
     align-items: center;
     justify-content: center;
     color:${colors.primary};
+`
+
+/* Search Form */
+export const SearchFormFullHeigth = styled.div`
+    height: 100vh;
+    background: rgba(79, 79, 79, 0.4);
+    position: absolute;
+    inset: 0;
+`
+
+export const SearchFormContainer = styled.div`
+    background: #ffffff;
+    transition: all ease 1s;
+`
+
+export const SearchFormWrapper = styled.div`
+    max-width: ${screens.lg};
+    margin: auto;
+    padding-top: 90px;
+    display: flex;
+    flex-direction: column;
+`
+
+export const FormRowInputWrapper = styled.div`
+    box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 16px;
+    display: flex;
+`
+
+export const InputWrapper = styled.div`
+    flex: 1 1 33.333333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding: 12px 30px;
+    border: 1px solid #F2F2F2;
+
+    &.is-focus{
+        border: 1px solid #333333;
+        border-radius: 16px;
+    }
+       
+
+    label{
+        font-weight: 800;
+        font-size: 9px;
+        text-transform: uppercase;
+    }
+
+    input{
+        font-weight: normal;
+        font-size: 14px;
+        line-height: 18px;
+        outline: none;
+        border:none;
+    }
+`
+export const ButtonSubmitWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ButtonSubmit = styled.button`
+    background: #EB5757;
+    box-shadow: 0px 1px 6px rgba(0, 0, 0, 0.1);
+    border-radius: 16px;
+    border:none;
+    padding:15px 25px;
+    color:white;
+    cursor: pointer;
+    font-weight: bold;
+    display: flex;
+    align-items: center;
+    gap:4px;
+`
+export const Input =styled.div`
+
+    ${InputWrapper}:focus & {
+            background: red;
+    }
+`
+
+export const FormRow =styled.div`
+    display: flex;
+    min-height: ${props => props.minHeight};
+`
+
+FormRow.defaultProps={
+    minHeight: 'auto'
+}
+
+export const LocationOptionList = styled.div`
+    flex: 1 1 33.333333%;
+    padding: 45px 30px;
+`
+
+export const LocationOption = styled.div`
+    color: #4F4F4F;
+`
+
+export const GuestCounterWrapper = styled.div`
+    flex: 1 1 33.333333%;
+    padding: 45px 30px;
+    padding-bottom: 70px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+export const GuestCounter = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+`
+
+export const GuestCounterLabel = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap:3px;
+`
+
+export const GuestCounterButtons = styled.div`
+    display: inline-flex;
+    justify-content: space-between;
+    gap:15px;
+    max-width: 90px;
+`
+export const GuestCounterButton = styled.button`
+    border: 1px solid #828282;
+    box-sizing: border-box;
+    border-radius: 4px;
+    color: #828282;
+    cursor: pointer;
+`
+export const EmptyColumn = styled.div`
+    flex: 1 1 33.333333%;
+    display: block;
+    padding: 45px 30px;
 `
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,154 @@
-import React from 'react'
+import React,{useState,useRef, useEffect} from 'react'
 import SearchIcon from '@material-ui/icons/Search';
-import {HeaderContainer,HeaderWrapper,
-LogoWrapper,Logo,SearchWidgetWrapper,InputLocation,InputGuests,SearchIconWrapper} from './HeaderComponents'
+import {
+    HeaderContainer,
+    HeaderWrapper,
+    LogoWrapper,
+    Logo,
+    SearchWidgetWrapper,
+    LocationText,
+    GuestsText,
+    SearchIconWrapper} from './HeaderComponents'
 import logo from '../../images/logo.svg'
 
+import {
+    SearchFormFullHeigth,
+    SearchFormContainer,
+    SearchFormWrapper,
+    FormRow,
+    FormRowInputWrapper,
+    InputWrapper,
+    ButtonSubmitWrapper,
+    ButtonSubmit,
+    LocationOptionList,
+    LocationOption,
+    GuestCounterWrapper,
+    GuestCounter,
+    GuestCounterLabel,
+    GuestCounterButtons,
+    GuestCounterButton,
+    EmptyColumn} from './HeaderComponents'
+
+function SearchForm(props){
+    
+    const [location, setLocation] = useState(props.location)
+    const [guests, setGuests] = useState(()=> {
+        const {children,adults} = props.guests
+        return children+adults
+    })
+    const [guestsObj, setGuestsObj] = useState(props.guests)
+    const [locations,setLocations] = useState([])
+    const refForm = useRef(null); 
+    const refInputLocation = useRef(null); 
+    
+    const handleChangeLocation = (e) => {
+        setLocation(e.target.value);
+    }
+    const handleFocusInput = (e) => {
+        const input = e.target;
+        const parent = input.parentElement;
+
+        parent.parentElement.childNodes.forEach(function(child){
+            child.classList.remove('is-focus');
+        })
+
+        parent.classList.add('is-focus');
+        
+    }
+    useEffect(() => {
+        
+        function handleClickOutside(e){
+
+            if(refForm.current && !refForm.current.contains(e.target)){
+                props.setIsFormVisible(false)
+            }
+        }
+        document.addEventListener('mousedown',handleClickOutside)
+        return () => {
+            document.removeEventListener('mousedown',handleClickOutside)
+        }
+    }, [])
+
+    useEffect(()=>{
+        if(refInputLocation.current) refInputLocation.current.focus();
+    },[])
+    
+    useEffect(()=>{
+        const {children,adults} = guestsObj
+
+        if(children || adults){
+            setGuests(children + adults)
+        }
+
+    },[guestsObj])
+
+    /* Render */
+
+    return (
+        <SearchFormFullHeigth>
+            <SearchFormContainer ref={refForm}>
+                <SearchFormWrapper>
+                        <FormRowInputWrapper>
+                            <InputWrapper active={true}>
+                                <label>Location</label>
+                                <input onFocus={handleFocusInput}  ref={refInputLocation} value={location} onChange={handleChangeLocation} placeholder="Add Location" />
+                            </InputWrapper>
+                            <InputWrapper>
+                                <label>Guest</label>
+                                <input onFocus={handleFocusInput} value={guests} placeholder="Add guests" min="0" type="number" onChange={(e)=>{ setGuests(e.target.value)}}/>
+                            </InputWrapper>
+                            <InputWrapper>
+                                <ButtonSubmitWrapper>
+                                    <ButtonSubmit><SearchIcon/>Search</ButtonSubmit>
+                                </ButtonSubmitWrapper>
+                            </InputWrapper>
+                        </FormRowInputWrapper>
+                        <FormRow minHeight="330px">
+                            <LocationOptionList>
+                                <LocationOption>Helsinki, Finland</LocationOption>
+                            </LocationOptionList>
+                            <GuestCounterWrapper>
+                                <GuestCounter>
+                                    <GuestCounterLabel>
+                                        <b>Adults</b>
+                                        Ages 13 or above
+                                    </GuestCounterLabel>
+                                    <GuestCounterButtons>
+                                        <GuestCounterButton>-</GuestCounterButton>
+                                        <div><b>0</b></div>
+                                        <GuestCounterButton>+</GuestCounterButton>
+                                    </GuestCounterButtons>
+                                </GuestCounter>
+                                <GuestCounter>
+                                    <GuestCounterLabel>
+                                        <b>Adults</b>
+                                        Ages 13 or above
+                                    </GuestCounterLabel>
+                                    <GuestCounterButtons>
+                                        <GuestCounterButton>-</GuestCounterButton>
+                                        <div><b>0</b></div>
+                                        <GuestCounterButton>+</GuestCounterButton>
+                                    </GuestCounterButtons>
+                                </GuestCounter>
+                            </GuestCounterWrapper>
+                            <EmptyColumn/>
+                        </FormRow>
+                </SearchFormWrapper>
+            </SearchFormContainer>
+        </SearchFormFullHeigth>
+    )
+}
+
+SearchForm.defaultProps = {
+    guests:{
+        children:0,
+        adults:0
+    }
+}
 function Header() {
+
+    const [isFormVisible,setIsFormVisible] = useState(false);
+
     return (
         <HeaderContainer>
             <HeaderWrapper>
@@ -13,14 +157,15 @@ function Header() {
                         <Logo src={logo}/>
                     </a>
                 </LogoWrapper>
-                <SearchWidgetWrapper>
-                    <InputLocation>Helsinki, Finland</InputLocation>
-                    <InputGuests>Add Guest</InputGuests>
+                <SearchWidgetWrapper onClick={() => setIsFormVisible(true)}>
+                    <LocationText>Helsinki, Finland</LocationText>
+                    <GuestsText>Add Guest</GuestsText>
                     <SearchIconWrapper>
                         <SearchIcon/>
                     </SearchIconWrapper>
                 </SearchWidgetWrapper>
             </HeaderWrapper>
+            {isFormVisible && <SearchForm location={"Helsinki, Finland"} setIsFormVisible={setIsFormVisible}/>}
         </HeaderContainer>
     )
 }
